abe7ffa2c273e86643ffc28a9b63f71b5a9cfd2b
Christos Koumpis <christoskoubis@yahoo.com>
2025-03-04 11:27:11 +0000

    Code improvements

--- src/MemoryUsage.php
+++ src/MemoryUsage.php
@@ -8,27 +8,29 @@ class MemoryUsage
     const BYTES_MB = 1024;
     const BYTES_KB = 1048576;
 
-    public static function getMemoryUsage(): float {
+    public static function getMemoryUsage(): float
+    {
         return memory_get_usage();
     }
 
-    public static function getPeakMemoryUsage(): float {
+    public static function getPeakMemoryUsage(): float
+    {
         return memory_get_peak_usage();
     }
 
-    public static function showMemory(int $bytes): string {
-        if($bytes >= self::BYTES_MB) {
+    public static function showMemory(int $bytes): string
+    {
+        if ($bytes >= self::BYTES_MB) {
             return self::formatMemory($bytes, self::BYTES_MB, ' MB');
-        }
-        elseif($bytes >= self::BYTES_KB) {
+        } elseif ($bytes >= self::BYTES_KB) {
             return self::formatMemory($bytes, self::BYTES_KB, ' KB');
-        }
-        else {
+        } else {
             return $bytes . " Bytes";
         }
     }
 
-    protected static function formatMemory(int $bytes, int $unitSize, string $unit): string {
-        return round($bytes/ $unitSize, 2) . ' ' . $unit;
+    protected static function formatMemory(int $bytes, int $unitSize, string $unit): string
+    {
+        return round($bytes / $unitSize, 2) . ' ' . $unit;
     }
 }
